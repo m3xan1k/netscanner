d2ec14553b7f6a63ba4247840ab4f3235fdcf3b9
m3xan1k <dev.serzh@gmail.com>
2021-12-04 18:07:56 +0300

    handle port arg

--- main.py
+++ main.py
@@ -21,19 +21,28 @@ SOCKET_TIMEOUT = 2
 def validate_port(ctx, param, port: str) -> str:
     if port.isnumeric():
         if in_valid_range(port):
-            return port
+            return [int(port)]
     regex = r'\d+:\d+'
     match = re.match(regex, port)
+    ports = None
     if match:
-        for number in port.split(':'):
+        ports = port.split(':')
+        if all(map(str.isdigit, ports)):
+            ports = range(*map(int, ports))
+    elif ',' in port:
+        ports = port.split(',')
+        if all(map(str.isdigit, ports)):
+            ports = [int(port) for port in ports]
+    if ports:
+        for number in ports:
             if not in_valid_range(number):
-                raise click.BadParameter('Port must be numeric or int:int')
-        return port
-    raise click.BadParameter('Port must be numeric or int:int')
+                raise click.BadParameter('Port must be int, int:int or int,int,int')
+        return ports
+    raise click.BadParameter('Port must be numeric or int:int or separated by comma')
 
 
-def in_valid_range(port: str) -> bool:
-    return int(port) in range(1, 65535)
+def in_valid_range(port: int) -> bool:
+    return port in range(1, 65535)
 
 
 def validate_target(ctx, param, target) -> str:
@@ -102,11 +111,8 @@ def scan_port(target: str, protocol: str, port_number: int) -> None:
 )
 def run(target, proto, port):
     # handle ports
-    if not port.isnumeric():
-        start, stop = map(int, port.split(':'))
-        ports = range(start, stop + 1)
-    else:
-        ports = [int(port)]
+    ports = port
+
     for protocol in proto:
         for port_number in ports:
             scan_port(target, protocol, port_number)
