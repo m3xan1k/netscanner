d4f6b3907b15c752fe6746dd40d2ae335fd100d3
m3xan1k <dev.serzh@gmail.com>
2021-12-04 17:41:39 +0300

    dns resolver, help, and func wrap

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ from IPy import IP
 import click
 
 
+class Protocol:
+    TCP = 'tcp'
+    UDP = 'udp'
+
+
+PROTOCOL_SOCKETS = {
+    'tcp': socket.SOCK_STREAM,
+    'udp': socket.SOCK_DGRAM,
+}
+SOCKET_TIMEOUT = 2
+
+
 def validate_port(ctx, param, port: str) -> str:
     if port.isnumeric():
         if in_valid_range(port):
@@ -29,7 +41,40 @@ def validate_target(ctx, param, target) -> str:
         IPv4Address(target)
         return target
     except AddressValueError:
+        try:
+            target = socket.getaddrinfo(target, 0)[0][4][0]
+            return target
+        except:
+            pass
         raise click.BadParameter('Not correct target')
+
+
+def resolve_domain_to_ipv4_address(domain: str) -> str:
+    pass
+
+
+def scan_port(target: str, protocol: str, port_number: int) -> None:
+    try:
+        sock = socket.socket(socket.AF_INET, PROTOCOL_SOCKETS[protocol])
+        sock.settimeout(SOCKET_TIMEOUT)
+
+        # behaviour depends on protocol
+        if protocol == Protocol.TCP:
+            sock.connect((target, port_number))
+            click.echo(
+                click.style(
+                    f'[+] Port {port_number} is open',
+                    fg='green'
+                )
+            )
+        elif protocol == Protocol.UDP:
+            sock.sendto(b'Hello', (target, port_number))
+            click.echo(f'[+] Sending UDP data to {port_number}')
+        sock.close()
+
+    except Exception as e:
+        click.echo(e, err=True)
+        print(f'[+] Port {port_number} is closed')
 
 
 @click.command()
@@ -38,6 +83,7 @@ def validate_target(ctx, param, target) -> str:
     prompt=True,
     type=click.UNPROCESSED,
     callback=validate_target,
+    help='ip address or domain name',
 )
 @click.option(
     '--proto',
@@ -45,12 +91,14 @@ def validate_target(ctx, param, target) -> str:
     multiple=True,
     show_choices=True,
     default=['tcp'],
+    help='tcp or udp',
 )
 @click.option(
     '--port',
     type=click.UNPROCESSED,
     callback=validate_port,
     prompt=True,
+    help='80 or 40000:50000',
 )
 def run(target, proto, port):
     # handle ports
@@ -58,35 +106,10 @@ def run(target, proto, port):
         start, stop = map(int, port.split(':'))
         ports = range(start, stop + 1)
     else:
-        ports = [port]
-    
-    # handle protocol
-    PROTOCOLS = {
-        'tcp': socket.SOCK_STREAM,
-        'udp': socket.SOCK_DGRAM,
-    }
-
-
+        ports = [int(port)]
     for protocol in proto:
         for port_number in ports:
-            try:
-                sock = socket.socket(socket.AF_INET, PROTOCOLS[protocol])
-                sock.settimeout(5)
-                if protocol == 'tcp':
-                    sock.connect((target, port_number))
-                    click.echo(
-                        click.style(
-                            f'[+] Port {port_number} is open',
-                            fg='green'
-                        )
-                    )
-                elif protocol == 'udp':
-                    sock.sendto(b'Hello', (target, port_number))
-                    click.echo(f'[+] Sending UDP data to {port_number}')
-                sock.close()
-            except Exception as e:
-                click.echo(e, err=True)
-                print(f'[+] Port {port_number} is closed')
+            scan_port(target, protocol, port_number)
 
 
 if __name__ == '__main__':
